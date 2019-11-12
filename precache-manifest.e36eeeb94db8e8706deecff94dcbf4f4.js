18452d2a6e8d77872218216ecd21dffbc27f125d
Meda Serenaite <meda.serenaite@designory.com>
2019-11-12 16:41:25 -0600

    Updates

---
+++ precache-manifest.e36eeeb94db8e8706deecff94dcbf4f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d209e5023735c30962623e1728173ce",
+    "url": "/medaserenaite/advent-calendar.git/index.html"
+  },
+  {
+    "revision": "e4220424ef1f3d6fb21a",
+    "url": "/medaserenaite/advent-calendar.git/static/css/main.69b1a6c7.chunk.css"
+  },
+  {
+    "revision": "f86ea3c5e791e5132d4c",
+    "url": "/medaserenaite/advent-calendar.git/static/js/2.89548203.chunk.js"
+  },
+  {
+    "revision": "e4220424ef1f3d6fb21a",
+    "url": "/medaserenaite/advent-calendar.git/static/js/main.eac338e6.chunk.js"
+  },
+  {
+    "revision": "80e4e076352ddc6da90c",
+    "url": "/medaserenaite/advent-calendar.git/static/js/runtime~main.130961f0.js"
+  }
+]);
