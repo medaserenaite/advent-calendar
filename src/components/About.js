dfaa0b46cc8f3abb73d12252578c92b5a701b4e2
Meda Serenaite <39907965+medaserenaite@users.noreply.github.com>
2019-11-12 16:50:31 -0600

    removes copy

--- src/components/About.js
+++ src/components/About.js
@@ -18,16 +18,7 @@ export default class Day extends Component {
   render() {
     return (
       <div className="About">
-            <button className="backButton" onClick={this.props.goBack.bind(this)}>back</button>
-            <p>Hey, boifren!</p>
-            <p>I made you an advent calendar so you (maybe) have fun while Christmas is slowly sneaking up</p>
-
-            <p>What to expext?</p>
-            <p>Everyday after midnight, a new day will become available for you to click on!<br/>You can click on it right after the clock dings OR in the morning OR whenever you feel like it!</p>
-
-            <p>What's inside the Click?</p>
-            <p>I tried to collect some of my favorite pictures of our time together and remember some my favorite moments with you.<br/>After you read OR not read the whole thing, at the end, I will reveal the icon of the day, which will tell you, which gift you can open that day!</p>
-            <button className="backButton" onClick={this.props.goBack.bind(this)}>back</button>
+            
       </div>
     );
   }
